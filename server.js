0ae307cf11b47206371460569db90df41343b684
Sachinthaka Ayeshmantha <it21219320@my.sliit.lk>
2024-04-27 15:53:27 +0530

    innitialized service

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connectDB } from "./config/db.js";
+
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+//Routes
+
+app.get('/', (req, res) => res.send('Course Management Service is running'));
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT, async () => {
+    await connectDB();
+    console.log(`Service is up and running on port: http://localhost:${PORT}`);
+})
+
+export default app;
